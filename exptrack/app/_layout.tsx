d597ddb34d3c37c6230cf83a4a4f91e84153b11d
idityaGE <am44910606@gmail.com>
2025-10-22 14:24:41 +0530

    route group

--- exptrack/app/_layout.tsx
+++ exptrack/app/_layout.tsx
@@ -1,17 +1,41 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "./global.css"
+import { AuthProvider, useAuth } from "@/store/auth-context";
+import { useEffect } from "react";
+
+const RouteGuard = ({ children }: { children: React.ReactNode }) => {
+  const { user, isLoading } = useAuth();
+  const router = useRouter();
+  // app/profile/[user].tsx
+  const segments = useSegments();  // segments = ["profile", "[user]"]
+
+  useEffect(() => {
+    const isAuthGroup = segments[0] === "auth";
+    if (!user && !isAuthGroup && !isLoading)
+      router.replace("/auth/login");
+    else if (user && isAuthGroup && !isLoading)
+      router.replace("/");
+  }, [user, segments])
+
+  return <>{children}</>
+}
+
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="auth" options={{ headerShown: false }} />
-        </Stack>
-      </SafeAreaProvider>
+      <AuthProvider>
+        <SafeAreaProvider>
+          <RouteGuard>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="auth" options={{ headerShown: false }} />
+            </Stack>
+          </RouteGuard>
+        </SafeAreaProvider>
+      </AuthProvider>
     </GestureHandlerRootView>
   )
 }
--- exptrack/store/auth-context.tsx
+++ exptrack/store/auth-context.tsx
@@ -1,2 +1,81 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { deleteItemAsync, getItemAsync, setItemAsync } from "expo-secure-store"
 
+export interface User {
+  userId: string,
+  name: string,
+  email: string,
+}
+
+interface AuthContextType {
+  user: User | null,
+  token: string | null,
+  login: (user: User, token: string) => Promise<void>,
+  logout: () => Promise<void>,
+  isLoading: boolean,
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const login = async (userData: User, authToken: string) => {
+    setUser(userData);
+    setToken(authToken);
+    await setItemAsync('authToken', authToken);
+    await setItemAsync('userData', JSON.stringify(userData));
+  }
+
+  const logout = async () => {
+    setUser(null);
+    setToken(null);
+    await deleteItemAsync('authToken');
+    await deleteItemAsync('userData');
+  }
+
+  useEffect(() => {
+
+    const loadAuthState = async () => {
+      try {
+        const storedToken = await getItemAsync('authToken');
+        const storedUserData = await getItemAsync('userData');
+
+        if (storedToken && storedUserData) {
+          setToken(storedToken);
+          setUser(JSON.parse(storedUserData));
+        }
+      } catch (error) {
+        console.error("Failed to load auth state:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    loadAuthState();
+  }, [])
+
+  const value: AuthContextType = {
+    user,
+    token,
+    login,
+    logout,
+    isLoading
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
